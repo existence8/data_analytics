92cbf137a71e6efff94939195f663df34ed71e74
Brian Luo <existence8@gmail.com>
2021-09-11 20:13:51 +0700

    filter 100words and include bad

--- data_analytics.py
+++ data_analytics.py
@@ -9,6 +9,7 @@ with open ('reviews.txt', 'r') as f:
 			print(count)
 	print('留言數一共', len(data), '筆')
 
+#隨機抽選列印(抽選復習)
 import random
 pick= random.randint(0, 1000000)
 print(data[pick])
@@ -19,3 +20,19 @@ for d in data:
 	sum_len = sum_len + len(d) #也可以寫成 sum_len +=len(d)
 
 print('留言的平均長度為:', sum_len/len(data))
+
+#篩選功能(一)：計算小於100字的留言數
+short = []
+for d in data:
+	if len(d) < 100:
+		short.append(d)
+print('小於100字的留言共有', len(short), '筆')
+print('例如', short[0])
+
+#篩選功能(二): 找出含有bad的留言
+bad = []
+for d in data:
+	if 'bad' in d:
+		bad.append(d)
+print('含有bad的留言一共有', len(bad), '筆')
+print('例如', bad[0])
